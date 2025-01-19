8f415d724452fe08c4b40b1c6e7790a2b683ea15
sijie.sun <sijie.sun@smartx.com>
2025-01-19 22:18:35 +0800

    Revert "fix(dayjs): update dayjs import to use ES module version."
    This reverts commit 0e763f7af0f87a66722cb0aa865a43823912c363.

--- src/components/HomeFooter.vue
+++ src/components/HomeFooter.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { FooterData, Icon, isExternal } from '../types'
 
-import dayjs from 'dayjs/esm'
+import dayjs from 'dayjs'
 
 // 使用 defineProps 定义属性
 const props = defineProps<{ Footer_Data: FooterData }>()
